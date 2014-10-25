2c9457a13c11835557fce5d76a0940ede85c5501
Tim Whitacre <tim@argyllstudios.com>
2014-10-25 15:31:44 -0400

    After install

--- Readme.md
+++ Readme.md
@@ -9,6 +9,15 @@ $ sh setup.sh
 
 > Note: This will automatically create a `dotfiles_old` folder and backup any exsisting files.
 
+
+## Get VIM Ready
+
+```sh
+$ vim # Any file will do
+:BundleInstall
+```
+
+
 ## Other Notes
 
 * Sublime Text 3 - [Predawn Theme](https://github.com/jamiewilson/predawn)
--- setup.sh
+++ setup.sh
@@ -8,19 +8,19 @@
 
 dir=~/dotfiles/templates                    # dotfiles directory
 olddir=~/dotfiles_old             # old dotfiles backup directory
-files="bashrc gitconfig tmux.conf zshrc oh-my-zsh"    # list of files/folders to symlink in homedir
+files="bashrc gitconfig tmux.conf zshrc vimrc"    # list of files/folders to symlink in homedir
 
 ##########
 
 # create dotfiles_old in homedir
 echo "Creating $olddir for backup of any existing dotfiles in ~"
 mkdir -p $olddir
-echo "...done"
+echo "___________________________________________________________________"
 
 # change to the dotfiles directory
 echo "Changing to the $dir directory"
 cd $dir
-echo "...done"
+echo "___________________________________________________________________"
 
 # move any existing dotfiles in homedir to dotfiles_old directory, then create symlinks 
 for file in $files; do
@@ -28,4 +28,10 @@ for file in $files; do
     mv ~/.$file ~/dotfiles_old/
     echo "Creating symlink to $file in home directory."
     ln -s $dir/$file ~/.$file
+    echo "___________________________________________________________________"
 done
+
+# Copy over Tim's theme
+echo "Copying Tim's ZSH Theme"
+cp ~/dotfiles/extras/tim.zsh-theme ~/.oh-my-zsh/themes/tim.zsh-theme
+echo "___________________________________________________________________"
